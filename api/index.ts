fa4b70a66d34b5e29434033f7b5a915b5ad1fde1
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-14 13:06:35 +0700

    v1.3

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from "express";
-
 const app: Express = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Official Javascript implementation of the Reforged Public API");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("[server]: Server is running at http://localhost:3000")
 });
+
+module.exports = app;
