07c9672685acc4c9e7831cae312066084232486f
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-16 08:52:57 +0700

    v1.8
    Migrate to Prisma ORM

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-import sequelize from './db';
-import Profile from './profile';
-
+const prisma = new PrismaClient()
 const app = express();
 const port = 5000;
 
@@ -14,23 +13,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: false }).then(() => {
-  console.log('[server] database and tables created');
-});
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Official Javascript implementation of the Reforged Public API");
 });
 
-app.get("/v1/profile", async (req: Request, res: Response) => {
-  try {
-    const profiles = await Profile.findAll();
-    res.json(profiles);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching profiles' });
-  }
-});
+app.get('/v1/profile', async (req, res) => {
+  const data = await prisma.mmoprofiles_playerdata.findMany()
+  res.json(data)
+})
 
 app.listen(port, () => {
   console.log(`[server] server is running at http://localhost:${port}`);
