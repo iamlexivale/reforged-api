20e969003b4f94c37200a99ab6888112143618b5
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-16 10:25:21 +0700

    v1.8.4

--- api/index.ts
+++ api/index.ts
@@ -13,13 +13,88 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Official Javascript implementation of the Reforged Public API");
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    res.status(200).json({
+      message: 'Official Javascript implementation of the Reforged Public API',
+      docs: 'https://docs.reforged.world'
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: 'An error occurred while fetching data',
+      details: error
+    });
+  }
 });
 
-app.get('/v1/profile', async (req, res) => {
-  const data = await prisma.mmoprofiles_playerdata.findMany();
-  res.json(data);
+app.get('/v1/users', async (req: Request, res: Response) => {
+  try {
+    const data = await prisma.mmoprofiles_playerdata.findMany();
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ error: 'users not found' });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: 'An error occurred while fetching /v1/users',
+      details: error
+    });
+  }
+});
+
+app.get('/v1/user/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const data = await prisma.mmoprofiles_playerdata.findUnique({
+      where: { uuid: id }
+    });
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ error: `user ${id} not found` });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred while fetching /v1/user/${id}`,
+      details: error
+    });
+  }
+});
+
+app.get('/v1/profiles', async (req: Request, res: Response) => {
+  try {
+    const data = await prisma.mmocore_playerdata.findMany();
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ error: 'profiles not found' });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: 'An error occurred while fetching /v1/profiles',
+      details: error
+    });
+  }
+});
+
+app.get('/v1/profile/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const data = await prisma.mmocore_playerdata.findUnique({
+      where: { uuid: id }
+    });
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ error: `profile ${id} not found` });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred while fetching /v1/profile/${id}`,
+      details: error
+    });
+  }
 });
 
 app.listen(port, () => {
