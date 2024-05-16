d1fd82de214c480e875a495e118120973a576019
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-16 07:46:40 +0700

    v1.7

--- api/db.ts
+++ api/db.ts
@@ -6,7 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: 3306,
-    dialect: 'mariadb'
+    dialect: 'mariadb',
+    dialectModule: require('mariadb')
   }
 );
 
